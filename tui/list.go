b254354760e94961e987638395d2bdcebdf4229f
cultab <rroarck@gmail.com>
2023-02-28 03:55:52 +0200

    feat(tui): toggle include expired assingments

--- tui/list.go
+++ tui/list.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"assignments/assignment"
-	"assignments/cmd/flags"
 	"assignments/config"
 	"assignments/course"
 
@@ -42,7 +41,7 @@ func NewList(config config.Config) listModel {
 	}
 	m.list.Title = "Εργασίες"
 	m.list.SetShowStatusBar(true)
-    statusTime := time.Second * 2
+	statusTime := time.Second * 2
 	m.list.StatusMessageLifetime = statusTime
 	m.list.SetStatusBarItemName("Εργασία", "Εργασίες")
 	m.list.SetSpinner(spinner.Dot)
@@ -55,6 +54,7 @@ func NewList(config config.Config) listModel {
 			m.keys.toggleHideCourse,
 			m.keys.toggleHideAssignment,
 			m.keys.saveConfig,
+			m.keys.toggleIncludeExpired,
 		}
 	}
 	return m
@@ -65,6 +65,7 @@ type keyBinds struct {
 	toggleHideCourse     key.Binding
 	toggleHidden         key.Binding
 	saveConfig           key.Binding
+	toggleIncludeExpired key.Binding
 }
 
 func newKeyBinds() keyBinds {
@@ -85,6 +86,10 @@ func newKeyBinds() keyBinds {
 			key.WithKeys("s", "σ"),
 			key.WithHelp("s|σ", "Αποθήκευση"),
 		),
+		toggleIncludeExpired: key.NewBinding(
+			key.WithKeys("i", "ι"),
+			key.WithHelp("i|ι", "Include expired"), // TODO: greek help description
+		),
 	}
 }
 
@@ -93,10 +98,15 @@ func (m listModel) Init() tea.Cmd {
 		m.list.StartSpinner(),
 		getAssignments,
 		mockGetAssignments,
+        updateTitleCmd,
 	)
 }
 
 var docStyle = lip.NewStyle().Margin(1, 2)
+
+type updateTitleMsg struct{}
+
+func updateTitleCmd() tea.Msg { return updateTitleMsg{} }
 
 type writeConfigMsg struct{}
 
@@ -104,7 +114,7 @@ func writeConfigCmd() tea.Msg { return writeConfigMsg{} }
 
 type updateMsg struct{}
 
-func updateCmd() tea.Msg { return updateMsg{} }
+func updateItemsCmd() tea.Msg { return updateMsg{} }
 
 func (m listModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
@@ -123,41 +133,48 @@ func (m listModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if !ok {
 				log.Print("Type Assertion failed")
 			}
-			// toggle hidden
-			hidden := false
-			for hidden_ass := range m.config.Options.ExcludedAssignments {
-				if hidden_ass == i.assignment.ID {
-					hidden = true
+			// toggle excluded
+			excluded := false
+			for excluded_assignment_ID := range m.config.Options.ExcludedAssignments {
+				if i.assignment.ID == excluded_assignment_ID {
+					excluded = true
 				}
 			}
-			if hidden {
+            var statusCmd tea.Cmd
+			if excluded {
 				delete(m.config.Options.ExcludedAssignments, i.assignment.ID)
+                statusCmd = m.list.NewStatusMessage("Επανέφερες την εργασία " + i.assignment.Title + ".")
 			} else {
 				m.config.Options.ExcludedAssignments[i.assignment.ID] = []string{} // HACK: uhh what do we put here again?
+                statusCmd = m.list.NewStatusMessage("Έκρυψες την εργασία " + i.assignment.Title + ".")
 			}
-			statusCmd := m.list.NewStatusMessage("Έκρυψες την εργασία " + i.assignment.Title + ".")
-			return m, tea.Batch(updateCmd, statusCmd)
+			return m, tea.Batch(updateItemsCmd, statusCmd)
 		case key.Matches(msg, m.keys.toggleHideCourse):
 			i, ok := m.list.SelectedItem().(item)
 			if !ok {
 				log.Print("Type Assertion failed")
 			}
-			hidden := false
-			for hidden_ass := range m.config.Options.ExcludedCourses {
-				if hidden_ass == i.assignment.Course.ID {
-					hidden = true
+			excluded := false
+			for excluded_course_ID := range m.config.Options.ExcludedCourses {
+				if i.assignment.Course.ID == excluded_course_ID {
+					excluded = true
 				}
 			}
-			if hidden {
+            var statusCmd tea.Cmd
+			if excluded {
 				delete(m.config.Options.ExcludedCourses, i.assignment.Course.ID)
+                statusCmd = m.list.NewStatusMessage("Επανέφερες τις εργασίες του μαθήματος " + i.assignment.Course.Name + ".")
 			} else {
 				m.config.Options.ExcludedCourses[i.assignment.Course.ID] = struct{}{}
+                statusCmd = m.list.NewStatusMessage("Έκρυψες τις εργασίες του μαθήματος " + i.assignment.Course.Name + ".")
 			}
-			statusCmd := m.list.NewStatusMessage("Έκρυψες τις εργασίες του μαθήματος " + i.assignment.Course.Name + ".")
-			return m, tea.Batch(updateCmd, statusCmd)
+			return m, tea.Batch(updateItemsCmd, statusCmd)
 		case key.Matches(msg, m.keys.toggleHidden):
 			m.showHidden = !m.showHidden
-			return m, updateCmd
+			return m, tea.Batch(updateItemsCmd, updateTitleCmd)
+		case key.Matches(msg, m.keys.toggleIncludeExpired):
+			m.config.Options.IncludeExpired = !m.config.Options.IncludeExpired
+			return m, tea.Batch(updateItemsCmd, updateTitleCmd)
 		}
 	case tea.WindowSizeMsg:
 		h, v := docStyle.GetFrameSize()
@@ -169,21 +186,23 @@ func (m listModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 		var new_items []list.Item
 		for _, item := range m.cache {
-			var hidden = false
-			for hidden_ass := range m.config.Options.ExcludedAssignments {
-				if hidden_ass == item.assignment.ID {
-					hidden = true
-					break
-				}
-			}
-			for hidden_course := range m.config.Options.ExcludedCourses {
-				if hidden_course == item.assignment.Course.ID {
-					hidden = true
-					break
-				}
+			hide := false
+			if item.shouldHideAssignment(m.config.Options.ExcludedAssignments) {
+				hide = true
 			}
 
-			if hidden == m.showHidden {
+			if item.shouldHideCourse(m.config.Options.ExcludedCourses) {
+				hide = true
+			}
+
+			// if the deadline has passed and we don't include expired OR we show hidden
+			// hide this item
+			if item.shouldHideExpired() && (!m.config.Options.IncludeExpired || m.showHidden) {
+				hide = true
+			}
+
+			// log.Println(item.assignment.ID, "hidden? ", hide, " because ", item.hideReason)
+			if hide == m.showHidden {
 				new_items = append(new_items, item)
 			}
 
@@ -195,11 +214,23 @@ func (m listModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.cache = append(m.cache, it.(item))
 		}
 		log.Print("Loaded assignments")
-        if len(msg) > 5 { // NOTE: mockGetAssignments returns 5, this is a hack
+		if len(msg) > 5 { // NOTE: mockGetAssignments returns 5, this is a hack
 			m.list.StopSpinner()
 		}
 		statusCmd := m.list.NewStatusMessage("Φόρτωση επιτυχής!")
-		return m, tea.Batch(updateCmd, statusCmd)
+		return m, tea.Batch(updateItemsCmd, statusCmd)
+	case updateTitleMsg:
+		var title string
+		if m.showHidden {
+			title += "Κρυμμένες εργασίες"
+		} else {
+			title += "Εργασίες"
+		}
+		if m.config.Options.IncludeExpired {
+			title += " | Εμφανίζονται Εκπρόθεσμες"
+		}
+		m.list.Title = title
+		return m, nil
 	case errorMsg:
 		log.Print(msg.err)
 		return m, nil
@@ -217,10 +248,44 @@ func (m listModel) View() string {
 
 type item struct {
 	assignment assignment.Assignment
+	hideReason string
+}
+
+func (i *item) shouldHideAssignment(excludedAssignments map[string][]string) bool {
+	for hidden_ass := range excludedAssignments {
+		if hidden_ass == i.assignment.ID {
+			i.hideReason = "κρυμένη εργασία"
+			return true
+		}
+	}
+	return false
+}
+
+func (i *item) shouldHideCourse(excludedCourses map[string]struct{}) bool {
+	for hidden_course := range excludedCourses {
+		if hidden_course == i.assignment.Course.ID {
+			i.hideReason = "εργασία κρυμένου μαθήματος"
+			return true
+		}
+	}
+
+	return false
+}
+
+func (i *item) shouldHideExpired() bool {
+	if i.isExpired() {
+		i.hideReason = "εκπρόθεσμη"
+		return true
+	}
+	return false
 }
 
 func (i item) FilterValue() string { // TODO: keybinds to change this func to others
 	return i.assignment.Title
+}
+
+func (i item) isExpired() bool {
+	return i.assignment.Deadline.Before(time.Now())
 }
 
 var ( // {{{
@@ -235,6 +300,7 @@ var ( // {{{
 	titleStyle = baseStyle.Copy().
 			PaddingLeft(2).
 			UnsetForeground().
+			MaxWidth(47).
 			Bold(true)
 	lateStyle = baseStyle.Copy().
 			Foreground(lip.Color("1"))
@@ -258,7 +324,7 @@ func (itemD itemDelegate) Render(w io.Writer, m list.Model, index int, listItem 
 		return
 	}
 
-	var title, course, date string
+	var title, course, date, hideReason string
 
 	title = titleStyle.Render(i.assignment.Title)
 	course = baseStyle.Render(i.assignment.Course.Name)
@@ -266,7 +332,7 @@ func (itemD itemDelegate) Render(w io.Writer, m list.Model, index int, listItem 
 	if i.assignment.IsSent {
 		date = doneStyle.Render("Παραδώθηκε πριν της:")
 	} else {
-		if i.assignment.Deadline.Before(time.Now()) {
+		if i.isExpired() {
 			date = lateStyle.Render("Εκπρόθεσμη:         ")
 		} else {
 			date = baseStyle.Render("Παράδωση εώς:       ")
@@ -275,7 +341,12 @@ func (itemD itemDelegate) Render(w io.Writer, m list.Model, index int, listItem 
 
 	date += baseStyle.Render(i.assignment.Deadline.Format("02/01/2006 15:04:05"))
 
-	result := lip.JoinVertical(lip.Left, title, course, date)
+	if i.hideReason != "" {
+		hideReason = baseStyle.Render("Κρυμένη γιατί είναι " + i.hideReason)
+	}
+
+	result := lip.JoinVertical(lip.Left, title, date, course)
+	result = lip.JoinHorizontal(lip.Top, result, hideReason)
 
 	if index == m.Index() {
 		result = hoverStyle.Render(result)
@@ -318,8 +389,6 @@ func getAssignments() tea.Msg {
 		return errorMsg{err}
 	}
 
-	flags.Read(opts, creds)
-
 	err = config.Ensure(opts, creds)
 	if err != nil {
 		return errorMsg{err}
@@ -328,6 +397,7 @@ func getAssignments() tea.Msg {
 	// get ALL assignments
 	opts.ExcludedAssignments = make(map[string][]string)
 	opts.ExcludedCourses = make(map[string]struct{})
+    opts.IncludeExpired = true
 
 	a, err := assignment.Get(opts, creds)
 	if err != nil {
